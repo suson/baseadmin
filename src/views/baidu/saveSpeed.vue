71b5e47932daaf875708d1f89005f2d2931b3b83
suson <350015235@qq.com>
2018-11-28 07:45:18 +0800

    addpage

--- src/views/baidu/saveSpeed.vue
+++ src/views/baidu/saveSpeed.vue
@@ -1,135 +1,163 @@
 <template>
-<div class="app-container">
+  <div class="app-container">
     <el-row>
-  <el-col :span="24"><div class="grid-content bg-purple">
-      <label>简介</label>
-        <p>1：百度排名任务是快速提升网站在百度上关键字排名，请添加百度前50名的关键字！提升手机排名请用手机百度功能。</p>
-        <p>2：任务总数代表这个任务一共能做多少次，每天次数代表每天可以做的数量。</p> 
-        <p>3：每天次数建议是关键字指数的十分之一左右，具体多少根据排名高低可以适当加减。<br></p>
-        <p>4：百度排名功能需要长期使用才会见效，如果只是偶尔使用效果很小。</p>
-        <p><br></p>
-      <label>基础设置</label>
-      </div></el-col>
-</el-row>
-    
+      <el-col :span="24">
+        <div class="grid-content bg-purple">
+          <label>简介</label>
+          <p>1：百度排名任务是快速提升网站在百度上关键字排名，请添加百度前50名的关键字！提升手机排名请用手机百度功能。</p>
+          <p>2：任务总数代表这个任务一共能做多少次，每天次数代表每天可以做的数量。</p>
+          <p>3：每天次数建议是关键字指数的十分之一左右，具体多少根据排名高低可以适当加减。<br></p>
+          <p>4：百度排名功能需要长期使用才会见效，如果只是偶尔使用效果很小。</p>
+          <p><br></p>
+          <label>基础设置</label>
+        </div>
+      </el-col>
+    </el-row>
+
     <el-row :gutter="20">
-  <el-col :span="2"><div class="grid-content bg-purple"></div></el-col>
-  <el-col :span="12"><div class="grid-content bg-purple">
-      
-<el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
-  <el-form-item label="任务备注" prop="name">
-    <el-input v-model="ruleForm.name" placeholder='备注名称'></el-input>
-    <span class="help-block info">请输入任务的备注名，以便自己识别</span>
-  </el-form-item>
-  
-    <el-form-item label="关键字" prop="keyword">
-    <el-input v-model="ruleForm.keyword" placeholder='添加需要刷的关键字'></el-input>
-    <span class="help-block tips">添加需要刷的关键字</span>
-  </el-form-item>
-  
-    <el-form-item label="域名" prop="url">
-    <el-input v-model="ruleForm.url" placeholder='例：http://www.baidu.com'></el-input>
-    <span class="help-block">网站域名</span>
-  </el-form-item>
-  <el-form-item>
-    <el-button type="primary" @click="submitForm('ruleForm')">{{save_tip}}</el-button>
-    <el-button @click="resetForm('ruleForm')">重置</el-button>
-    <el-button @click="goBack()">返回列表</el-button>
-  </el-form-item>
-</el-form>
-      
-      </div></el-col>
-      <el-col :span="2"><div class="grid-content bg-purple"></div></el-col>
-      <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-  <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-</el-row>
-</div>
+      <el-col :span="2">
+        <div class="grid-content bg-purple"></div>
+      </el-col>
+      <el-col :span="12">
+        <div class="grid-content bg-purple">
+
+          <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+            <el-form-item label="任务备注" prop="name">
+              <el-input v-model="ruleForm.name" placeholder='备注名称'></el-input>
+              <!-- <span class="help-block info">请输入任务的备注名，以便自己识别</span> -->
+            </el-form-item>
+
+            <el-form-item label="关键字" prop="keyword">
+              <el-input v-model="ruleForm.keyword" placeholder='添加需要刷的关键字'></el-input>
+              <!-- <span class="help-block tips">添加需要刷的关键字</span> -->
+            </el-form-item>
+
+            <el-form-item label="域名" prop="url">
+              <el-input v-model="ruleForm.url" placeholder='例：http://www.baidu.com'></el-input>
+              <!-- <span class="help-block">网站域名</span> -->
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="submitForm('ruleForm')">{{save_tip}}</el-button>
+              <el-button @click="resetForm('ruleForm')">重置</el-button>
+              <el-button @click="goBack()">返回列表</el-button>
+            </el-form-item>
+          </el-form>
+
+        </div>
+      </el-col>
+      <el-col :span="2">
+        <div class="grid-content bg-purple"></div>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple"></div>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple"></div>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 <style>
-.el-row {
+  .el-row {
     margin-bottom: 20px;
+
     &:last-child {
       margin-bottom: 0;
     }
   }
+
   .el-col {
     border-radius: 4px;
   }
+
   .bg-purple-dark {
     background: #99a9bf;
   }
+
   /* .bg-purple {
     background: #d3dce6;
   } */
   .bg-purple-light {
     background: #e5e9f2;
   }
+
   .grid-content {
     border-radius: 4px;
     min-height: 36px;
   }
+
   .row-bg {
     padding: 10px 0;
     background-color: #f9fafc;
   }
+
 </style>
 
 <script>
-import { getInfo,save } from '@/api/task'
+  import {
+    getInfo,
+    save
+  } from "@/api/task";
 
   export default {
     created() {
-        console.log(this.$route)
-        if (this.$route.meta.isEdit) {
-            this.ruleForm.urlid = this.$route.params.id;
-            this.getInfo(this.ruleForm.urlid);
-            this.save_tip = '保存';
-        }
+      console.log(this.$route);
+      if (this.$route.meta.isEdit) {
+        this.ruleForm.urlid = this.$route.params.id;
+        this.getInfo(this.ruleForm.urlid);
+        this.save_tip = "保存";
+      }
     },
     data() {
+        var validateUrl = (rule,value,callback) => {
+            var urlreg = /^(?=^.{3,255}$)(http(s)?:\/\/)?(www\.)?[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+(:\d+)*(\/\w+\.\w+)*([\?&amp;]\w+=\w*)*$/;
+            if (!urlreg.test(value)) {
+                callback(new Error('请输入正确的网址'));
+            } else {
+                callback();
+            }
+        }
       return {
-        save_tip:'立即创建',
-        type:'baidu_speed',
+        save_tip: "立即创建",
+        type: "baidu_speed",
         ruleForm: {
-          id: '',
-          name: '',
-          keyword: '',
-          url: '',
-          desc: ''
+          id: "",
+          name: "",
+          keyword: "",
+          url: "",
+          desc: ""
         },
         rules: {
-          name: [
-            { required: true, message: '请输入任务备注', trigger: 'blur' },
-            { min: 3, max: 50, message: '长度在 3 到 50 个字符', trigger: 'blur' }
+          name: [{
+              required: true,
+              message: "请输入任务备注",
+              trigger: "blur"
+            },
+            {
+              min: 3,
+              max: 50,
+              message: "长度在 3 到 50 个字符",
+              trigger: "blur"
+            }
           ],
-          region: [
-            { required: true, message: '请选择活动区域', trigger: 'change' }
-          ],
-          date1: [
-            { type: 'date', required: true, message: '请选择日期', trigger: 'change' }
-          ],
-          date2: [
-            { type: 'date', required: true, message: '请选择时间', trigger: 'change' }
-          ],
-          type: [
-            { type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change' }
-          ],
-          resource: [
-            { required: true, message: '请选择活动资源', trigger: 'change' }
-          ],
-          desc: [
-            { required: true, message: '请填写活动形式', trigger: 'blur' }
+          url:[
+            {
+              required: true,
+              message: "请输入网址",
+              trigger: "blur"
+            },
+            { validator: validateUrl, trigger: 'blur' }
           ]
         }
       };
     },
     methods: {
       submitForm(formName) {
-        this.$refs[formName].validate((valid) => {
+        this.$refs[formName].validate(valid => {
           if (valid) {
             this.save();
           } else {
-            console.log('error submit!!');
+            console.log("error submit!!");
             return false;
           }
         });
@@ -138,34 +166,35 @@ import { getInfo,save } from '@/api/task'
         this.$refs[formName].resetFields();
       },
       goBack() {
-        window.history.length > 1
-            ? this.$router.go(-1)
-            : this.$router.push('/baidu_task/speed')
+        window.history.length > 1 ?
+          this.$router.go(-1) :
+          this.$router.push("/baidu_task/speed");
       },
       getInfo(id) {
-          let i = {
-              "urlid" : id
-          }
-          i = JSON.stringify(i);
-          getInfo(i).then(response => {
-            // console.log(response);
-                this.ruleForm = response.data;
-            })
+        let i = {
+          urlid: id
+        };
+        i = JSON.stringify(i);
+        getInfo(i).then(response => {
+          // console.log(response);
+          this.ruleForm = response.data;
+        });
       },
       save() {
-          let i = this.ruleForm;
-          i.type = this.type;
-          i = JSON.stringify(i);
-          save(i).then(response => {
-              if (response.error > -1) {
-                  this.$message({
-                        type: 'success',
-                        message: '操作成功!'
-                    });
-                  this.$router.push('/baidu_task/speed');
-              }
-            })
+        let i = this.ruleForm;
+        i.type = this.type;
+        i = JSON.stringify(i);
+        save(i).then(response => {
+          if (response.error > -1) {
+            this.$message({
+              type: "success",
+              message: "操作成功!"
+            });
+            this.$router.push("/baidu_task/speed");
+          }
+        });
       }
     }
-  }
+  };
+
 </script>
--- src/views/baidu/speed.vue
+++ src/views/baidu/speed.vue
@@ -1,248 +1,305 @@
 <template>
-<div class="app-container">
-  <el-table
-    ref="multipleTable"
-    empty-text="暂无数据"
-    v-loading="listLoading"
-    border
-    :data="listData"
-    tooltip-effect="dark"
-    style="width: 100%"
-    @selection-change="handleSelectionChange">
-    <el-table-column
-      type="selection"
-      width="50">
-    </el-table-column>
-    <el-table-column
-      label="id"
-      width="120">
-      <template slot-scope="scope">{{ scope.row.urlid }}</template>
-    </el-table-column>
-    <el-table-column
-      label="标题"
-      width="220">
-      <template slot-scope="scope">{{ scope.row.name }}</template>
-    </el-table-column>
-    <el-table-column
-      label="网址"
-      show-overflow-tooltip>
-      <template slot-scope="scope">{{ scope.row.turl | filterUrl('url')}}</template>
-    </el-table-column>
-    <el-table-column
-      label="关键字"
-      width="220">
-      <template slot-scope="scope">{{ scope.row.turl | filterUrl('keyword')}}</template>
-    </el-table-column>
-    <el-table-column
-      label="今日分享"
-      width="120">
-      <template slot-scope="scope">{{ scope.row.tdclick }}</template>
-    </el-table-column>
-    <el-table-column
-      label="状态"
-      width="120">
-      <template slot-scope="scope">{{scope.row.online | urlStatusName}}-{{scope.row.free | shareStatusName}}</template>
-    </el-table-column>
-    <el-table-column
-      label="操作"
-      width="120">
-      <template slot-scope="scope"><el-button
-          size="mini"
-          @click="edit(scope.row)">编辑</el-button></template>
-    </el-table-column>
-  </el-table>
-  <div style="margin-top: 20px">
-    <el-button @click="operate(0)">停止</el-button>
-    <el-button @click="operate(1)">开始</el-button>
-    <el-button @click="delTip()">删除</el-button>
+  <div class="app-container">
+    <el-row>
+      <el-col :span="24">
+        <div class="grid-content bg-purple">
+          <el-button @click="addTask()" size="small" type="primary" plain>创建新任务</el-button>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24">
+        <div class="grid-content bg-purple">
+          <el-table ref="multipleTable" empty-text="暂无数据" v-loading="listLoading" border :data="listData"
+            tooltip-effect="dark" style="width: 100%" @selection-change="handleSelectionChange">
+            <el-table-column type="selection" width="50">
+            </el-table-column>
+            <el-table-column label="id" width="120">
+              <template slot-scope="scope">{{ scope.row.urlid }}</template>
+            </el-table-column>
+            <el-table-column label="标题" width="220">
+              <template slot-scope="scope">{{ scope.row.name }}</template>
+            </el-table-column>
+            <el-table-column label="网址" show-overflow-tooltip>
+              <template slot-scope="scope">{{ scope.row.turl | filterUrl('url')}}</template>
+            </el-table-column>
+            <el-table-column label="关键字" width="220">
+              <template slot-scope="scope">{{ scope.row.turl | filterUrl('keyword')}}</template>
+            </el-table-column>
+            <el-table-column label="今日分享" width="80">
+              <template slot-scope="scope">{{ scope.row.tdclick }}</template>
+            </el-table-column>
+            <el-table-column label="状态" width="120">
+              <template slot-scope="scope">{{scope.row.online | urlStatusName}}-{{scope.row.free | shareStatusName}}</template>
+            </el-table-column>
+            <el-table-column label="操作" width="160">
+              <template slot-scope="scope">
+                <el-button size="mini" type="primary" @click="edit(scope.row)">编辑</el-button>
+                <el-button size="mini" @click="open(scope.row)">开通</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+          <div style="margin-top: 20px">
+            <el-button-group>
+            <el-button size="small" type="primary" icon="el-icon-remove-outline" @click="operate(0)">停止</el-button>
+            <el-button size="small" type="primary" icon="el-icon-caret-right" @click="operate(1)">开始</el-button>
+            <el-button size="small" type="danger" icon="el-icon-delete" @click="delTip()">删除</el-button>
+            </el-button-group>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
+<el-dialog
+  title="提示"
+  :visible.sync="centerDialogVisible"
+  width="60%"
+  >
+  <span>需要注意的是内容是默认不居中的</span>
+  <span slot="footer" class="dialog-footer">
+    <el-button @click="centerDialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="centerDialogVisible = false">确 定</el-button>
+  </span>
+</el-dialog>
   </div>
-</div>
 </template>
+<style>
+  .el-row {
+    margin-bottom: 20px;
 
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+
+  .el-col {
+    border-radius: 4px;
+  }
+
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+
+  /* .bg-purple {
+    background: #d3dce6;
+  } */
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+
+</style>
 <script>
-import { getList,del,operate } from '@/api/task'
+  import {
+    getList,
+    del,
+    operate
+  } from '@/api/task'
 
-export default {
+  export default {
     data() {
-        return {
-            listLoading:true,
-            listData: [],
-            multipleSelection: []
-        }
+      return {
+        listLoading: true,
+        centerDialogVisible:false,
+        listData: [],
+        multipleSelection: []
+      }
     },
     created() {
-        this.fetchData()
+      this.fetchData()
     },
     filters: {
-        urlStatusName: function (value) {
-            var name = '';
-            switch(value.toString()) {
-                case '0':
-                name = '离线';
-                break;
-                case '1':
-                name = '在线';
-                break;
+      urlStatusName: function (value) {
+        var name = '';
+        switch (value.toString()) {
+          case '0':
+            name = '离线';
+            break;
+          case '1':
+            name = '在线';
+            break;
 
-            }
-            return name;
-        },
-        shareStatusName:function(value) {
-            var name = '';
-            switch(value.toString()) {
-                case '0':
-                name = '未分享';
-                break;
-                case '1':
-                name = '分享中';
-                break;
-
-            }
-            return name;
-        },
-        filterUrl:function(data,type) {
-            data = data.split("|");
-            switch(type) {
-                case 'url':
-                    return data[0] || '';
-                break;
-                case 'keyword':
-                    return data[1] || '';
-                break;
-            }
         }
+        return name;
+      },
+      shareStatusName: function (value) {
+        var name = '';
+        switch (value.toString()) {
+          case '0':
+            name = '未分享';
+            break;
+          case '1':
+            name = '分享中';
+            break;
+
+        }
+        return name;
+      },
+      filterUrl: function (data, type) {
+        data = data.split("|");
+        switch (type) {
+          case 'url':
+            return data[0] || '';
+            break;
+          case 'keyword':
+            return data[1] || '';
+            break;
+        }
+      }
     },
     methods: {
-        toggleSelection(rows) {
-            if (rows) {
-                rows.forEach(row => {
-                this.$refs.multipleTable.toggleRowSelection(row);
-                });
-            } else {
-                this.$refs.multipleTable.clearSelection();
-            }
-        },
-        handleSelectionChange(val) {
-            this.multipleSelection = val;
-        },
-        fetchData() {
-            this.listLoading=false;
-            var i = {
-                "type":"baidu_speed"
-            };
-            i = JSON.stringify(i);
-            getList(i).then(response => {
-                // console.log(response);
-                this.listData = response.urls;
-                this.listLoading = false
-            })
-        },
-        delTip:function() {
-            // console.log(this.multipleSelection);
-            var ids=[];
-            for (var i in this.multipleSelection) {
-                ids.push(this.multipleSelection[i].urlid);
-            }
-            if (ids.length < 1) {
-                this.$message({
-                    message: '请选择任务',
-                    type: 'error',
-                    duration: 3 * 1000
-                })
-                return false;
-            }
-            this.$confirm(
-            '删除则不可恢复，确定删除？',
-            '提示',
-            {
-                confirmButtonText: '确定',
-                cancelButtonText: '取消',
-                type: 'warning'
-            }
-            ).then(() => {
-                this.del(ids);
-                
-            }).catch(() => {
-                // this.$message({
-                //     type: 'info',
-                //     message: '已取消删除'
-                // });
-            });
-            console.log(1);
-
-            return;
-        },
-        del:function(ids){
-            console.log(ids);
-            var i = {
-                "ids":ids
-            }
-            i = JSON.stringify(i);
-            del(i).then(response => {
-                if(response.error > -1 ) {
-                    for (var i in ids) {
-                        // console.log(ids[i])
-                        for (var j in this.listData) {
-                            if (ids[i] == this.listData[j].urlid) {
-                                this.listData.splice(j,1);
-                                break;
-                            }
-                        }
-                    }
-                    this.$message({
-                        type: 'success',
-                        message: '删除成功!'
-                    });
-                } else {
-                    this.$message({
-                        message: response.msg,
-                        type: 'error',
-                        duration: 3 * 1000
-                    })
-                }
-            })
-        },
-        operate:function(type) {
-            var ids=[];
-            for (var i in this.multipleSelection) {
-                ids.push(this.multipleSelection[i].urlid);
-            }
-            if (ids.length < 1) {
-                this.$message({
-                    message: '请选择任务',
-                    type: 'error',
-                    duration: 3 * 1000
-                })
-                return false;
-            }
-            var i = {
-                "free":type,
-                "ids":ids
-            }
-            i = JSON.stringify(i);
-            operate(i).then(response => {
-                if(response.error > -1 ) {
-                    for (var i in ids) {
-                        // console.log(ids[i])
-                        for (var j in this.listData) {
-                            if (ids[i] == this.listData[j].urlid) {
-                                this.listData[j].free = type;
-                                break;
-                            }
-                        }
-                    }
-                } else {
-                    this.$message({
-                        message: response.msg,
-                        type: 'error',
-                        duration: 3 * 1000
-                    })
-                }
-            })
-        },
-        edit(item) {
-            this.$router.push({path: '/baidu_task/edit/'+item.urlid})
+      toggleSelection(rows) {
+        if (rows) {
+          rows.forEach(row => {
+            this.$refs.multipleTable.toggleRowSelection(row);
+          });
+        } else {
+          this.$refs.multipleTable.clearSelection();
         }
+      },
+      handleSelectionChange(val) {
+        this.multipleSelection = val;
+      },
+      fetchData() {
+        this.listLoading = false;
+        var i = {
+          "type": "baidu_speed"
+        };
+        i = JSON.stringify(i);
+        getList(i).then(response => {
+          // console.log(response);
+          this.listData = response.urls;
+          this.listLoading = false
+        })
+      },
+      delTip: function () {
+        // console.log(this.multipleSelection);
+        var ids = [];
+        for (var i in this.multipleSelection) {
+          ids.push(this.multipleSelection[i].urlid);
+        }
+        if (ids.length < 1) {
+          this.$message({
+            message: '请选择任务',
+            type: 'error',
+            duration: 3 * 1000
+          })
+          return false;
+        }
+        this.$confirm(
+          '删除则不可恢复，确定删除？',
+          '提示', {
+            confirmButtonText: '确定',
+            cancelButtonText: '取消',
+            type: 'warning'
+          }
+        ).then(() => {
+          this.del(ids);
+
+        }).catch(() => {
+          // this.$message({
+          //     type: 'info',
+          //     message: '已取消删除'
+          // });
+        });
+        console.log(1);
+
+        return;
+      },
+      del: function (ids) {
+        console.log(ids);
+        var i = {
+          "ids": ids
+        }
+        i = JSON.stringify(i);
+        del(i).then(response => {
+          if (response.error > -1) {
+            for (var i in ids) {
+              // console.log(ids[i])
+              for (var j in this.listData) {
+                if (ids[i] == this.listData[j].urlid) {
+                  this.listData.splice(j, 1);
+                  break;
+                }
+              }
+            }
+            this.$message({
+              type: 'success',
+              message: '删除成功!'
+            });
+          } else {
+            this.$message({
+              message: response.msg,
+              type: 'error',
+              duration: 3 * 1000
+            })
+          }
+        })
+      },
+      addTask: function () {
+        this.$router.push('/baidu_task/add')
+      },
+      operate: function (type) {
+        var ids = [];
+        for (var i in this.multipleSelection) {
+          ids.push(this.multipleSelection[i].urlid);
+        }
+        if (ids.length < 1) {
+          this.$message({
+            message: '请选择任务',
+            type: 'error',
+            duration: 3 * 1000
+          })
+          return false;
+        }
+        var i = {
+          "free": type,
+          "ids": ids
+        }
+        i = JSON.stringify(i);
+        operate(i).then(response => {
+          if (response.error > -1) {
+            for (var i in ids) {
+              // console.log(ids[i])
+              for (var j in this.listData) {
+                if (ids[i] == this.listData[j].urlid) {
+                  this.listData[j].free = type;
+                  break;
+                }
+              }
+            }
+            this.$message({
+              type: "success",
+              message: "操作成功!"
+            });
+          } else {
+            this.$message({
+              message: response.msg,
+              type: 'error',
+              duration: 3 * 1000
+            })
+          }
+        })
+      },
+      edit(item) {
+        this.$router.push({
+          path: '/baidu_task/edit/' + item.urlid
+        })
+      },
+      open(item) {
+          this.$refs.multipleTable.clearSelection();
+          this.$refs.multipleTable.toggleRowSelection(item,true);
+          this.centerDialogVisible = true
+      }
     }
-}
+  }
+
 </script>
